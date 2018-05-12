d005da7f524ddc1103793d42900fb7c7f1d0b4b2
Daniel Yu <34182773+daniel-yu-goed@users.noreply.github.com>
2018-05-12 23:13:27 +0800

    create initial project

---
+++ src/main/java/gensesis/exam/java/App.java
@@ -0,0 +1,13 @@
+package gensesis.exam.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

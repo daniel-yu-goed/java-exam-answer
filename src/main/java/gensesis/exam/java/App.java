62079d0b61aded1867d89682ab6662b13d7a2117
Daniel Yu <34182773+daniel-yu-goed@users.noreply.github.com>
2018-05-13 23:29:30 +0800

    update App and AppTest

--- src/main/java/gensesis/exam/java/App.java
+++ src/main/java/gensesis/exam/java/App.java
@@ -1,13 +1,76 @@
 package gensesis.exam.java;
 
+import java.util.HashMap;
+
+import com.genesis.exams.slot.Evaluator;
+import com.genesis.exams.slot.Reel;
+import com.genesis.exams.slot.SlotMachine;
+import com.genesis.exams.slot.SpinResult;
+import com.genesis.exams.slot.Spinner;
+import com.genesis.exams.slot.Symbol;
+
 /**
- * Hello world!
+ * This is the main method for a {@link com.genesis.exams.slot.SlotMachine SlotMachine}.
+ * @author Daniel Yu
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    private static long BET = 100;
+
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        Reel[] reels = setupReels();
+        HashMap<String, Integer> winningCombination = setupWinningCombination();   
+
+        SpinResult spinResult = runSlotMachine(BET, reels, winningCombination);
+        Symbol[] symbols = spinResult.getSymbols();
+        
+        System.out.println("The symbols after spinning is: " + getStringOfSymbols(symbols));
+        System.out.println("The pay-out is: " + spinResult.getPayout());
+    }
+    
+    public static SpinResult runSlotMachine(long bet, Reel[] reels, HashMap<String, Integer> winningCombination) {        
+        Spinner spinner = new ConcreteSpinner();        
+        Evaluator evaluator = new ConcreteEvaluator(winningCombination);
+        SlotMachine slotMachine = new SlotMachine(reels, spinner, evaluator);
+        
+        return slotMachine.spin(bet);
+    }
+    
+    private static Reel[] setupReels() {
+        // initialize symbols
+        Symbol symbolA = new Symbol("A", 1);
+        Symbol symbolB = new Symbol("B", 1);
+        Symbol symbolC = new Symbol("C", 1);
+        Symbol symbolX = new Symbol("X", 2);
+        Symbol symbolY = new Symbol("Y", 3);
+        Symbol symbolZ = new Symbol("Z", 4);
+        
+        // initialize reels
+        Reel reelOne   = new Reel(new Symbol[] {symbolA, symbolB, symbolC, symbolX, symbolY, symbolZ});
+        Reel reelTwo   = new Reel(new Symbol[] {symbolZ, symbolY, symbolX, symbolA, symbolB, symbolC});
+        Reel reelThree = new Reel(new Symbol[] {symbolA, symbolB, symbolC, symbolX, symbolY, symbolZ});
+
+        return new Reel[] {reelOne, reelTwo, reelThree};
+    }
+    
+    private static HashMap<String, Integer> setupWinningCombination() {
+        return new HashMap<String, Integer>() {
+            {
+                put("A,A,A",  20);
+                put("B,B,B",  20);
+                put("C,C,C",  20);
+                put("A,B,C",  30);
+            }
+        };
+    }
+
+    private static String getStringOfSymbols(Symbol[] symbols) {
+        String symbolString = "";
+        for(Symbol symbol: symbols) {
+            symbolString += symbol.getName() + ",";
+        }
+        return symbolString.substring(0, symbolString.length()-1);
     }
 }
--- src/test/java/gensesis/exam/java/AppTest.java
+++ src/test/java/gensesis/exam/java/AppTest.java
@@ -2,19 +2,58 @@ package gensesis.exam.java;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import com.genesis.exams.slot.Reel;
+import com.genesis.exams.slot.SpinResult;
+import com.genesis.exams.slot.Symbol;
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private final long BET = 100;
+    private final Integer MULTIPLIER_TWENTY = 20;
+    private final Integer MULTIPLIER_THIRTY = 30;
+
+    private Reel[] reels;
+    private HashMap<String, Integer> winningCombination;
+    private Long[] allPayouts;
+    
+    @Before
+    public void setUp() throws Exception {
+        // initialize symbols
+        Symbol symbolA = new Symbol("A", 1);
+        Symbol symbolB = new Symbol("B", 1);
+        Symbol symbolC = new Symbol("C", 1);
+        Symbol symbolX = new Symbol("X", 2);
+        Symbol symbolY = new Symbol("Y", 3);
+        Symbol symbolZ = new Symbol("Z", 4);
+        
+        // initialize reels
+        Reel reelOne   = new Reel(new Symbol[] {symbolA, symbolB, symbolC, symbolX, symbolY, symbolZ});
+        Reel reelTwo   = new Reel(new Symbol[] {symbolZ, symbolY, symbolX, symbolA, symbolB, symbolC});
+        Reel reelThree = new Reel(new Symbol[] {symbolA, symbolB, symbolC, symbolX, symbolY, symbolZ});
+        reels = new Reel[] {reelOne, reelTwo, reelThree};
+        
+        winningCombination = new HashMap<String, Integer>() {
+            {
+                put("A,A,A",  MULTIPLIER_TWENTY);
+                put("B,B,B",  MULTIPLIER_TWENTY);
+                put("C,C,C",  MULTIPLIER_TWENTY);
+                put("A,B,C",  MULTIPLIER_THIRTY);
+            }
+        };
+        
+        allPayouts = new Long[] {Long.valueOf("0"), BET*MULTIPLIER_TWENTY, BET*MULTIPLIER_THIRTY};
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void testApp()
     {
-        assertTrue( true );
+        SpinResult spinResult = App.runSlotMachine(BET, reels, winningCombination);
+        assertTrue(Arrays.asList(allPayouts).contains(spinResult.getPayout()));
     }
 }
